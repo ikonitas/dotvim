609aaec0aa20d522df484d7056d567513bd66ca1
Edvinas Jurevicius <ikonitas@gmail.com>
2016-05-27 14:34:26 +0100

    Change ctags script

--- plugin/ctags.py
+++ plugin/ctags.py
@@ -8,12 +8,18 @@ import sys
 PROJECT_DIR = os.environ.get('PROJECT_DIR')
 paths = ''
 
-for directory in sys.path:
-    if os.path.isdir(directory) and not directory.startswith('/usr/lib'):
-        paths += '{} '.format(directory)
-command = (
-    'ctags -R --fields=+l --languages=python --python-kinds=-iv -f {0}/.git/.tags {0} {1}'.format(
-        PROJECT_DIR, paths
+if PROJECT_DIR:
+    for directory in sys.path:
+        if os.path.isdir(directory) and not directory.startswith('/usr/lib'):
+            paths += '{} '.format(directory)
+
+    old_tag_path = '{}/.git/.tags'.format(PROJECT_DIR)
+    if os.path.exists(old_tag_path):
+        remove_command = ('rm {}/.git/.tags'.format(PROJECT_DIR))
+        subprocess.Popen(remove_command, shell=True)
+
+    command = (
+        'ctags -R --fields=+l --languages=python --python-kinds=-iv -f '
+        '{0}/.git/.tags {0} {1}'.format(PROJECT_DIR, paths)
     )
-)
-sp = subprocess.Popen(command, shell=True, stdout=subprocess.PIPE, preexec_fn=os.setsid)
+    sp = subprocess.Popen(command, shell=True, stdout=subprocess.PIPE, preexec_fn=os.setsid)
